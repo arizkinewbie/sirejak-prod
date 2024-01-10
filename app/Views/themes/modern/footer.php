737b7be3c690795f008a2ade3086f0278e5d40aa
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-01-10 11:09:03 +0700

    initial commit

---
+++ app/Views/themes/modern/footer.php
@@ -0,0 +1,15 @@
+	</div><!-- cotent-wrapper -->
+	</div><!-- cotent -->
+	</div><!-- site-content -->
+	<footer class="shadow">
+		<div class="footer-copyright">
+			<div class="wrapper">
+				<?php 
+					echo '© ', date('Y');
+				?>
+				<a href="https://lldikti3.kemdikbud.go.id/v6/" target="_blank">Lembaga Layanan Pendidikan Tinggi Wilayah III</a>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
